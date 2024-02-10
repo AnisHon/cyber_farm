bdc3bc96b252255de6bc204b28e3710d58539379
AnisHan_window <3137687133@qq.com>
2024-02-10 20:38:02 +0800

    update About Us add my farm

--- js/detail.js
+++ js/detail.js
@@ -1,71 +1,62 @@
-let minus = document.getElementById("minus");
-let add = document.getElementById("add");
-let count = document.getElementById("count")
-let total = document.getElementById("price");
-let single = Number(document.getElementById("single_price").innerHTML);
+$(() => {
+    const goods_detail = $(".goods_detail");
+    const price = $("#single_price")
+    const total_price = $("#price");
+    const count = $("#count");
+    const detail_text = goods_detail.children(".detail_text");
+    const p = detail_text.children("p");
+    const main_img = $(".goods_img .main");
 
 
-
-add.onclick = function() {
-    if (Number(count.value) < 10) {
-        count.value = Number(count.value) + 1;
-        change_price(Number(count.value));
-    }
-}
-
-minus.onclick = function() {
-    if (Number(count.value) > 1) {
-        count.value = Number(count.value) - 1;
-        change_price(Number(count.value));
-    }
-    
-}
-
-count.onkeydown = function(e) {
-    const keyCode = e.key;
-    let validate = isNumber(keyCode);
-    if (validate) {
-        change_price(Number(count.value + keyCode));
-    }
-    return validate || keyCode === "Backspace" || keyCode === "del";
-    
-}
-function isNumber(keyCode) {
-    // 数字
-    if (keyCode >= 0 && keyCode <= 10) {
-        if (Number(keyCode) === 0 && count.value === "") {
-            return false
+    $("#add").click((event) => {
+        if (Number(count.val()) + 1 <= 10) {
+            count.val(Number(count.val()) + 1 + '');
         }
-        return Number(count.value + keyCode) <= 10;
-        
+        updatePrice();
+    })
+    $("#minus").click((event) => {
+        if (Number(count.val()) - 1 > 0) {
+            count.val(Number(count.val()) - 1 + '');
+        }
+        updatePrice();
+    })
+
+    //change img
+    $(".goods_img .small").on("click", "img", (event) => {
+
+        if ($(event.target).is(".goods_img .little")) {
+            main_img.attr("src", $(event.target).attr("src"))
+        }
+    });
+
+//change text
+    detail_text.children("ul").on("click", "li", (event) => {
+        p.html(getDetailText($(event.target).attr("data-col")));
+        $(event.target).addClass("active_item").siblings().removeClass("active_item")
+    })
+
+
+    function getDetailText(num) {
+        return num + num + "****************"
     }
-    // Backspace, del, 左右方向键
-    return true;
 
-}
-
-
-const DETAIL = 1;
-const PRODUCER = 2;
-const COMMENT = 3;
-
-let cols = document.getElementsByClassName("col");
-let list = document.getElementsByClassName("col_list");
-for (let i = 0; i < cols.length; i++) {
-    cols[i].hidden = true;
-}
-cols[0].hidden = false;
-function changePage(num) {
-
-    for (let i = 0; i < cols.length; i++) {
-        cols[i].hidden = true;
-        list[i].classList.remove("active_item");
+    function updatePrice() {
+        total_price.html(Number(price.html()) * Number(count.val()) + "");
     }
-    let element = cols[num - 1];
-    element.hidden = false;
-    list[num - 1].classList.add("active_item");
-}
 
-function change_price(num) {
-    total.innerHTML = String(single * num);
-}
+
+    p.html(getDetailText(1));
+    updatePrice();
+
+});
+
+
+
+
+
+
+
+
+
+
+
